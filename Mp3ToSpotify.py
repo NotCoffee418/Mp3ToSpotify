909f30716e61b0bd688dc279066ca6228dc62b48
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2019-12-28 11:33:26 +0100

    Spotify Authentication using spotipy

--- Mp3ToSpotify.py
+++ Mp3ToSpotify.py
@@ -1,6 +1,10 @@
-from os import listdir, path
 import sys
+from os import listdir, path
 import datetime
+import spotipy
+import spotipy.util as util
+
+spotify = spotipy.Spotify()
 
 # Function to request bool inputs
 def bool_input(queryString):
@@ -23,6 +27,15 @@ def log_line(line):
     f=open(logFilePath, "a+")
     f.write(line + "\r\n")
     f.close()
+
+# Authenticate the user with spotify
+def spotify_authenticate():
+    username=""
+    while (username == ""):
+        username=input("Please enter your Spotify username:\n")
+    scope="user-library-read user-library-modify playlist-read-private playlist-modify-private user-read-private"
+    auth_token = util.prompt_for_user_token(username,scope,client_id='05794919be674441824fcb3267a22437',client_secret='7014411cbff3403285415679b1dea145',redirect_uri='http://localhost:5932/')
+    return spotipy.Spotify(auth=auth_token)
 
 ###
 # Import Script
@@ -48,3 +61,6 @@ while (path.isdir(mp3Dir) == False):
 
 ## Ask to check subdirectories
 includeSubdirectories = bool_input("Would you like to include subdirectories while searching for files?")
+
+## Authenticate with spotify & get spotify handle
+sp = spotify_authenticate()
---
+++ README.md
@@ -0,0 +1,8 @@
+### Instuctions:
+```
+pip3 install spotipy
+git clone https://github.com/NotCoffee418/Mp3ToSpotify.git
+cd Mp3ToSpotify
+python3 Mp3ToSpotify.py
+```
+Then follow the on-screen instructions and let the script do it's thing.
